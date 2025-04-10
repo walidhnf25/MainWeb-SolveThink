f429554537924b93b31657c86ca0564649b47a42
Walid Hanif Ataullah <walidhnf12@gmail.com>
2025-04-10 23:55:16 +0700

    perbaikan directory css pada code di tabler

--- resources/views/layouts/tabler.blade.php
+++ resources/views/layouts/tabler.blade.php
@@ -30,7 +30,7 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Template Stylesheet -->
-    <link href="css/style.css" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <!-- SweetAlert2 Stylesheet -->
     <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.4.35/dist/sweetalert2.min.css" rel="stylesheet">
